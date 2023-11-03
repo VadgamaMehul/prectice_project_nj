ca608833be0c0af15ac8b111faba870d04f58486
Mehul Vadgama <vadgama.mehul444@gmail.com>
2023-11-03 18:56:25 +0530

    Prectice project

---
+++ Server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.port;
+
+// Connect to MongoDB database using Mongoose.
+//get route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//Listen
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
